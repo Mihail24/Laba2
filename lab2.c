cb7c4d9679ba8791b289c4768a6477eca0b797b8
Mihail24 <mihanromanov@yandex.ru>
2016-06-01 16:09:51 +0300

    main

--- lab2.c
+++ lab2.c
@@ -2,3 +2,33 @@
 #include <math.h>
 
 void qwrt(double a, double b, double c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+	double a, b, c;
+	printf("Enter a, b, c: \n");
+	scanf("%lf%lf%lf", &a, &b, &c);
+	qwrt(a, b, c);
+	return 0;
+}
+
