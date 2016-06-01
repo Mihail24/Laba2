fc8cdf55427a631cbdad429d57d2c859072f127f
Mihail24 <mihanromanov@yandex.ru>
2016-06-01 16:06:15 +0300

    D>0

--- lab2.c
+++ lab2.c
@@ -15,3 +15,10 @@ void qwrt(double a, double b, double c)
 		x1 = -b / (2 * a);
 		printf("\nRoot: %lf\n", x1);
 	}
+	else if (d > 0)
+	{
+		x1 = (-b + sqrt(d)) / (2 * a);
+		x2 = (-b - sqrt(d)) / (2 * a);
+		printf("\nRoots: %lf %lf\n", x1, x2);
+	}
+}
